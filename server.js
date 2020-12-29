d3844f674de29ce2313e1828dbd92e0342abd84c
Brett Winterstrom <brettwinterstrom@yahoo.com>
2020-12-29 14:10:08 -0800

    made connection to mysql database

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '257789601Me$',
+    database: 'employee_trackerdb',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    connection.end();
+})
+
